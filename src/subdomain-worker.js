154154dd395ec66007043ba40f18f0293ba16dc2
Vladan Colovic <cvladan@gmail.com>
2025-08-01 08:59:02 +0200

    Insert frontend JS to override standard document.cookie

--- src/subdomain-worker.js
+++ src/subdomain-worker.js
@@ -27,6 +27,47 @@ export default {
     }
 
 
+    const contentType = originResponse.headers.get('content-type') || '';
+    if (contentType.includes('text/html')) {
+      const rewriter = new HTMLRewriter().on('head', {
+        element(element) {
+          element.prepend(`
+            <script>
+              (function() {
+                const newDomain = "${TARGET_DOMAIN}";
+                const originalCookieDescriptor = Object.getOwnPropertyDescriptor(Document.prototype, 'cookie');
+                if (!originalCookieDescriptor) {
+                    console.error("Could not get original cookie descriptor.");
+                    return;
+                }
+                Object.defineProperty(Document.prototype, 'cookie', {
+                    get: function() {
+                        return originalCookieDescriptor.get.call(document);
+                    },
+                    set: function(cookieString) {
+                        let modifiedCookieString = cookieString;
+                        const domainRegex = /;\\s*domain=[^;]+/i;
+                        if (domainRegex.test(cookieString)) {
+                            modifiedCookieString = cookieString.replace(domainRegex, \`; domain=\${newDomain}\`);
+                        } else {
+                            modifiedCookieString = \`\${cookieString}; domain=\${newDomain}\`;
+                        }
+                        originalCookieDescriptor.set.call(document, modifiedCookieString);
+                    }
+                });
+              })();
+            </script>
+          `, { html: true });
+        }
+      });
+      const rewrittenResponse = rewriter.transform(originResponse);
+      return new Response(rewrittenResponse.body, {
+        status: originResponse.status,
+        statusText: originResponse.statusText,
+        headers: newHeaders,
+      });
+    }
+
     return new Response(originResponse.body, {
       status: originResponse.status,
       statusText: originResponse.statusText,
