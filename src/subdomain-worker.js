a1d69ab4df7fa2d0c27016160e513ee81ae86e62
Vladan Colovic <cvladan@gmail.com>
2025-08-06 14:09:12 +0200

    This was about canonical link rewriting logic

--- src/subdomain-worker.js
+++ src/subdomain-worker.js
@@ -1,4 +1,5 @@
-const TARGET_DOMAIN = 'weddyplace.com';
+const COOKIE_DOMAIN = 'weddyplace.com';
+const CANONICAL_TARGET = 'https://www.weddyplace.com/karten';
 
 export default {
   async fetch(request) {
@@ -13,9 +14,9 @@ export default {
       if (name.toLowerCase() === 'set-cookie') {
         let rewrittenCookie = value.replace(/Path=[^;]+/gi, 'Path=/');
         if (/Domain=/i.test(rewrittenCookie)) {
-          rewrittenCookie = rewrittenCookie.replace(/Domain=[^;]+/gi, `Domain=${TARGET_DOMAIN}`);
+          rewrittenCookie = rewrittenCookie.replace(/Domain=[^;]+/gi, `Domain=${COOKIE_DOMAIN}`);
         } else {
-          rewrittenCookie += `; Domain=${TARGET_DOMAIN}`;
+          rewrittenCookie += `; Domain=${COOKIE_DOMAIN}`;
         }
         cookieHeaders.push(rewrittenCookie);
       }
@@ -29,37 +30,57 @@ export default {
 
     const contentType = originResponse.headers.get('content-type') || '';
     if (contentType.includes('text/html')) {
-      const rewriter = new HTMLRewriter().on('head', {
-        element(element) {
-          element.prepend(`
-            <script>
-              (function() {
-                const newDomain = "${TARGET_DOMAIN}";
-                const originalCookieDescriptor = Object.getOwnPropertyDescriptor(Document.prototype, 'cookie');
-                if (!originalCookieDescriptor) {
-                    console.error("Could not get original cookie descriptor.");
-                    return;
-                }
-                Object.defineProperty(Document.prototype, 'cookie', {
-                    get: function() {
-                        return originalCookieDescriptor.get.call(document);
-                    },
-                    set: function(cookieString) {
-                        let modifiedCookieString = cookieString;
-                        const domainRegex = /;\\s*domain=[^;]+/i;
-                        if (domainRegex.test(cookieString)) {
-                            modifiedCookieString = cookieString.replace(domainRegex, \`; domain=\${newDomain}\`);
-                        } else {
-                            modifiedCookieString = \`\${cookieString}; domain=\${newDomain}\`;
-                        }
-                        originalCookieDescriptor.set.call(document, modifiedCookieString);
-                    }
-                });
-              })();
-            </script>
-          `, { html: true });
-        }
-      });
+      const rewriter = new HTMLRewriter()
+        .on('head', {
+          element(element) {
+            element.prepend(`
+              <script>
+                (function() {
+                  const newDomain = "${COOKIE_DOMAIN}";
+                  const originalCookieDescriptor = Object.getOwnPropertyDescriptor(Document.prototype, 'cookie');
+                  if (!originalCookieDescriptor) {
+                      console.error("Could not get original cookie descriptor.");
+                      return;
+                  }
+                  Object.defineProperty(Document.prototype, 'cookie', {
+                      get: function() {
+                          return originalCookieDescriptor.get.call(document);
+                      },
+                      set: function(cookieString) {
+                          let modifiedCookieString = cookieString;
+                          const domainRegex = /;\\s*domain=[^;]+/i;
+                          if (domainRegex.test(cookieString)) {
+                              modifiedCookieString = cookieString.replace(domainRegex, \`; domain=\${newDomain}\`);
+                          } else {
+                              modifiedCookieString = \`\${cookieString}; domain=\${newDomain}\`;
+                          }
+                          originalCookieDescriptor.set.call(document, modifiedCookieString);
+                      }
+                  });
+                })();
+              </script>
+            `, { html: true });
+          }
+        })
+        .on('link[rel="canonical"]', {
+          element(element) {
+            const href = element.getAttribute('href');
+            if (href) {
+              try {
+                const originalUrl = new URL(href);
+                const targetUrl = new URL(CANONICAL_TARGET);
+                // Append the original path and query to the new target
+                const newPath = targetUrl.pathname + originalUrl.pathname + originalUrl.search;
+                element.setAttribute('href', targetUrl.origin + newPath);
+              } catch (e) {
+                // If href is not a valid URL, it might be a relative path
+                // In this case, we'll just append it to the target
+                const targetUrl = new URL(CANONICAL_TARGET);
+                element.setAttribute('href', targetUrl.origin + targetUrl.pathname + href);
+              }
+            }
+          }
+        });
       const rewrittenResponse = rewriter.transform(originResponse);
       return new Response(rewrittenResponse.body, {
         status: originResponse.status,
